5fdb25ab963c8fa6194a968190098c8f6205a248
tes <tressa0923@gmail.com>
2020-07-29 01:28:40 -0700

    Updates

---
+++ precache-manifest.488c0027685b7088f96790704f57455d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06826a0d8b4c99b306062484266ae732",
+    "url": "/scfreeclinic/index.html"
+  },
+  {
+    "revision": "c6d1c1bdfee329dfc4cc",
+    "url": "/scfreeclinic/static/css/main.36445554.chunk.css"
+  },
+  {
+    "revision": "26fa437fcb61d8630e4f",
+    "url": "/scfreeclinic/static/js/2.17ad3559.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/scfreeclinic/static/js/2.17ad3559.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6d1c1bdfee329dfc4cc",
+    "url": "/scfreeclinic/static/js/main.04b7f886.chunk.js"
+  },
+  {
+    "revision": "1c59c8e87472042d82ae",
+    "url": "/scfreeclinic/static/js/runtime-main.a948c81a.js"
+  },
+  {
+    "revision": "eef6590e5866da141ae4ccd0c1d02176",
+    "url": "/scfreeclinic/static/media/campus.eef6590e.jpg"
+  }
+]);
