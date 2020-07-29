1b8f67cecc10242cb3fb54d80cff472b74fbe48c
tes <tressa0923@gmail.com>
2020-07-29 02:53:55 -0700

    predeploy added

---
+++ build/precache-manifest.a4b8d7280c25110eedf2370a033b2488.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b86c0a3494a696c5a9b5aff688bfc8a",
+    "url": "/scfreeclinic/index.html"
+  },
+  {
+    "revision": "b934d47293995811f5c2",
+    "url": "/scfreeclinic/static/css/main.36445554.chunk.css"
+  },
+  {
+    "revision": "656f254725258eb8cb1b",
+    "url": "/scfreeclinic/static/js/2.804672dd.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/scfreeclinic/static/js/2.804672dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b934d47293995811f5c2",
+    "url": "/scfreeclinic/static/js/main.b768d8fc.chunk.js"
+  },
+  {
+    "revision": "58d410c4c8c18ada07e1",
+    "url": "/scfreeclinic/static/js/runtime-main.c10fae1e.js"
+  },
+  {
+    "revision": "eef6590e5866da141ae4ccd0c1d02176",
+    "url": "/scfreeclinic/static/media/campus.eef6590e.jpg"
+  }
+]);
