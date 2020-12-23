75a8353d582cc39c4b7a1ee775ea613840f2f167
tes <tressa0923@gmail.com>
2020-12-23 12:34:55 -0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a12e92e41623becf3198cae13dd68ff9.js"
+  "/precache-manifest.3f1442b1d16d9512116b68b48a05beb3.js"
 );
 
 self.addEventListener('message', (event) => {
